beb6777a13e9f70fbd20abb7a0a64e352ae90961
flybit <>
2018-05-25 10:45:53 -0400

    Use let instead of var

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const cardTypes = ['fa-anchor', 'fa-bicycle', 'fa-bolt', 'fa-bomb', 'fa-cube', '
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
