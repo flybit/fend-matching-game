ef56b2e51c4c717095a25675ff076ba7b5ac235f
flybit <>
2018-05-25 10:47:24 -0400

    Call createDeck() to see if things work as expected

--- js/app.js
+++ js/app.js
@@ -45,6 +45,8 @@ function createDeck() {
     container.appendChild(ul);
 }
 
+createDeck();
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
